42a9bb2caae5528d7b923697b4b721ae68e45b6c
Shubham Sharma <shubham09sharma12@gmail.com>
2021-07-15 18:38:26 -0400

    bootstraped giphy project

---
+++ js/contact.js
@@ -0,0 +1,42 @@
+// $(document).ready(function(){
+//     $("#sub").click(function(){
+//       alert("The paragraph was clicked.");
+//     });
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   $(function() {
+//     // Initialize form validation on the registration form.
+//     // It has the name attribute "registration"
+//     $("form[name='form-sub']").validate({
+//       // Specify validation rules
+//       rules: {
+//         // The key name on the left side is the name attribute
+//         // of an input field. Validation rules are defined
+//         // on the right side
+//         name: "required",
+//         message: "required"
+//       },
+//       // Specify validation error messages
+//       messages: {
+//         name: "Please enter your firstname",
+//         message: "dsndnsnd"
+//       },
+//       // Make sure the form is submitted to the destination defined
+//       // in the "action" attribute of the form when valid
+//       submitHandler: function(form) {
+//         form.submit();
+//       }
+//     });
+//   });
